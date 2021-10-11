74258c6472ce6bf8e41fb87724f01333563397f4
Aleksander Lengard <aleksander.lengard@gmail.com>
2021-10-11 22:01:04 +0200

    Lag til funksjon for å velge spørsmål fra forskjellige kategorier

--- src/App.js
+++ src/App.js
@@ -3,38 +3,19 @@ import { useState } from 'react'
 import QuizSetup from './components/QuizSetup'
 import QuizContainer from './components/QuizContainer'
 
-import storage from './storage/3-CSS.json'
-
 const App = () => {
-  const { questions } = storage.quiz
-  const quizCount = questions.length
-
-  const [numQuesions, setNumQuesions] = useState(1)
-  const [quizQuestions, setQuizQuestions] = useState([])
-
-  const getRandomQuestion = (arr) =>
-    arr.splice(Math.floor(Math.random() * arr.length), 1)[0]
-
-  const startQuiz = () => {
-    const selectQuestionsArr = [...questions]
-    for (let i = 0; i < numQuesions; i += 1) {
-      setQuizQuestions((arr) => [...arr, getRandomQuestion(selectQuestionsArr)])
-    }
-  }
+  const [questions, setQuestions] = useState([])
 
   const startNewQuiz = () => {
-    setQuizQuestions([])
+    setQuestions([])
   }
 
   return (
     <main className="w-3/5 mb-auto mt-10 m-auto py-6 flex flex-col space-y-6">
-      {quizQuestions.length === 0 ? (
-        <QuizSetup
-          questionData={{ quizCount, numQuesions }}
-          quizHandlers={{ startQuiz, setNumQuesions }}
-        />
+      {questions.length === 0 ? (
+        <QuizSetup setQuestions={setQuestions} />
       ) : (
-        <QuizContainer questions={quizQuestions} startNewQuiz={startNewQuiz} />
+        <QuizContainer questions={questions} startNewQuiz={startNewQuiz} />
       )}
     </main>
   )
--- src/components/QuizSetup.js
+++ src/components/QuizSetup.js
@@ -1,37 +1,84 @@
-const QuizSetup = ({ questionData, quizHandlers }) => {
-  const { startQuiz, setNumQuesions } = quizHandlers
-  const { quizCount, numQuesions } = questionData
+import { useState } from 'react'
+import { getCategories, getQuestionCount, getQuestions } from '../storage/db'
 
-  const updateNum = (event) => setNumQuesions(event.target.value)
-  const setSliderMax = () => setNumQuesions(quizCount)
-  const startQuizHandler = () => startQuiz()
+const QuizSetup = ({ setQuestions }) => {
+  const [categories, setCategories] = useState([])
+  const [availableQuestions, setAvailableQuestions] = useState(0)
+  const [numQuestions, setNumQuestions] = useState(1)
+
+  const updateNum = (event) => setNumQuestions(event.target.value)
+  const setSliderMax = () => setNumQuestions(availableQuestions)
+  const updateSilderParams = (count) => {
+    setAvailableQuestions(count)
+    if (numQuestions > count) setNumQuestions(count)
+  }
+  const startQuizHandler = () => {
+    setQuestions(getQuestions(numQuestions, categories))
+  }
+
+  const handleSelectCategories = (event) => {
+    let tempSelectedCategories = [...categories, event.target.id]
+    if (categories.includes(event.target.id))
+      tempSelectedCategories = categories.filter(
+        (category) => category !== event.target.id
+      )
+    setCategories(tempSelectedCategories)
+    updateSilderParams(getQuestionCount(tempSelectedCategories))
+  }
 
   return (
-    <section className="wrapper col w-1/2 m-auto animate-fade">
-      <h1 className="header">Hvor mange spørsmål?</h1>
-      <div className="items-baseline">
-        <input
-          type="range"
-          min="1"
-          max={quizCount}
-          value={numQuesions}
-          onChange={updateNum}
-          className="appearance-none w-full h-1 bg-gray-400 rounded outline-none slider-thumb"
-        />
-        <button type="button" className="btn-white" onClick={setSliderMax}>
-          Alle
-        </button>
-      </div>
-      <div className="items-baseline">
-        <p>
-          Start quiz med<span className="font-bold"> {numQuesions} </span>
-          spørsmål
-        </p>
-        <button type="button" onClick={startQuizHandler} className="btn-blue">
-          GO!
-        </button>
-      </div>
-    </section>
+    <>
+      <section className="wrapper col w-1/2 m-auto animate-fade">
+        <h1 className="header">Velg kategorier:</h1>
+
+        <ul className="ul-col-2 w-100">
+          {getCategories().map((cat, i) => (
+            <li key={i} className="p-1">
+              <input
+                id={cat}
+                name={cat}
+                type="checkbox"
+                onChange={handleSelectCategories}
+                className="mr-2"
+              />
+              <label htmlFor={cat}>{cat}</label>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      {categories.length > 0 && (
+        <section className="wrapper col w-1/2 m-auto animate-fade">
+          <h1 className="header">Hvor mange spørsmål?</h1>
+          <div className="items-baseline">
+            <input
+              type="range"
+              min="1"
+              max={availableQuestions}
+              value={numQuestions}
+              onChange={updateNum}
+              className="appearance-none w-full h-1 bg-gray-400 rounded outline-none slider-thumb"
+            />
+            <button type="button" className="btn-white" onClick={setSliderMax}>
+              Alle
+            </button>
+          </div>
+          <div className="items-baseline">
+            <p>
+              Start quiz med<span className="font-bold"> {numQuestions} </span>
+              spørsmål
+            </p>
+            <button
+              type="button"
+              onClick={startQuizHandler}
+              className="btn-blue"
+            >
+              GO!
+            </button>
+          </div>
+        </section>
+      )}
+    </>
   )
 }
 export default QuizSetup
